42efd5501522e99da08695850024f1f707a33154
BatuhanD7 <97726220+BatuhanD7@users.noreply.github.com>
2023-01-21 18:40:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,22 +20,22 @@ function dice(){
 
 switch (player1) {
   case 1:
-    document.querySelector(".img1").setAttribute("src" , "images/dice1.png");
+    document.querySelector(".img1").setAttribute("src" , "dice1.png");
     break;
   case 2:
-      document.querySelector(".img1").setAttribute("src" , "images/dice2.png");
+      document.querySelector(".img1").setAttribute("src" , "dice2.png");
     break;
   case 3:
-      document.querySelector(".img1").setAttribute("src" , "images/dice3.png");
+      document.querySelector(".img1").setAttribute("src" , "dice3.png");
     break;
   case 4:
-      document.querySelector(".img1").setAttribute("src" , "images/dice4.png");
+      document.querySelector(".img1").setAttribute("src" , "dice4.png");
     break;
   case 5:
-      document.querySelector(".img1").setAttribute("src" , "images/dice5.png");
+      document.querySelector(".img1").setAttribute("src" , "dice5.png");
     break;
   case 6:
-      document.querySelector(".img1").setAttribute("src" , "images/dice6.png");
+      document.querySelector(".img1").setAttribute("src" , "dice6.png");
     break;
 
   default:
@@ -43,22 +43,22 @@ switch (player1) {
 
 switch (player2) {
   case 1:
-    document.querySelector(".img2").setAttribute("src" , "images/dice1.png");
+    document.querySelector(".img2").setAttribute("src" , "dice1.png");
     break;
   case 2:
-      document.querySelector(".img2").setAttribute("src" , "images/dice2.png");
+      document.querySelector(".img2").setAttribute("src" , "dice2.png");
     break;
   case 3:
-      document.querySelector(".img2").setAttribute("src" , "images/dice3.png");
+      document.querySelector(".img2").setAttribute("src" , "dice3.png");
     break;
   case 4:
-      document.querySelector(".img2").setAttribute("src" , "images/dice4.png");
+      document.querySelector(".img2").setAttribute("src" , "dice4.png");
     break;
   case 5:
-      document.querySelector(".img2").setAttribute("src" , "images/dice5.png");
+      document.querySelector(".img2").setAttribute("src" , "dice5.png");
     break;
   case 6:
-      document.querySelector(".img2").setAttribute("src" , "images/dice6.png");
+      document.querySelector(".img2").setAttribute("src" , "dice6.png");
     break;
 
   default:
